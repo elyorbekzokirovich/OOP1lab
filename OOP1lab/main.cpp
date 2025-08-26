387100e816a02fda15f55b668baee4929484c2b2
Elyorbek <e.allanazarov@newuu.uz>
2025-08-26 14:27:35 +0500

    Initial Commit

---
+++ OOP1lab/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OOP1lab
+//
+//  Created by Elyorbek Allanazarov on 26/08/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    for(int i=0;i<5;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
